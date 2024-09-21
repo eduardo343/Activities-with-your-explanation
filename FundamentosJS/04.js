6d841ad8deb8204d1bbf4ef0f74f2314c328670b
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2024-09-20 20:12:20 -0600

    Update 04.js

--- FundamentosJS/04.js
+++ FundamentosJS/04.js
@@ -1,36 +1,36 @@
-// objetos
+// Objects
 
-//const nombre = "tablet" // estos quedaran desactivados porque utilizaremos lo que estamos codificando abajo
-//const precio = 300
-//const disponible = true
+// const nombre = "tablet"; // these will be commented out because we will use what we are coding below
+// const precio = 300; 
+// const disponible = true;
 
-//el crear objetos
-const producto = { /// a las llaves en ingles se les dice curl braces
-   nombre : "tablet ",
-   precio : 300,
-   disponible : true
-}
+// Creating objects
+const producto = { // curly braces in English
+   nombre: "tablet",
+   precio: 300,
+   disponible: true
+};
 
-console.log (producto)
-console.table(producto)// cuando usas el modo table se acomodan los elementos en modo table y es mejor visiblemente
+console.log(producto);
+console.table(producto); // when using table mode, elements are arranged in a table format, making it visually clearer
 
-console.log(producto.disponible)// para acceder a las propiedades usa estos comandos
-console.log(producto.nombre)//si sigues te aparecera mucha informacion de como usar comandos solo da click object y prototype
-console.log(producto.precio)// cuando pones el . significa que siempre accderas a un objeto
+console.log(producto.disponible); // to access properties, use these commands
+console.log(producto.nombre); // if you continue, you will see a lot of information on how to use commands; just click on object and prototype
+console.log(producto.precio); // using the dot means you will always access an object's properties
 
-//destructuring  //el destructuring sirve para sacar valores
-const { nombre, precio, disponible } = producto //puedes inventar bastantes cosas hasta la palabra imagen que tomara los valores de arriba
-console.log(nombre) // esto es lo mismo que usar console.log solo que en esta ocasion usas const y las {}
-console.log(precio)
-console.log(disponible)
+// Destructuring // destructuring is used to extract values
+const { nombre, precio, disponible } = producto; // you can create many variables, including one called imagen that will take values from above
+console.log(nombre); // this is the same as using console.log, but here you use const and {}
+console.log(precio);
+console.log(disponible);
 
-//object literal enhacement
-const autenticado = true
-const usuario = 'alan'
-const password = 'tugfa2.0'
+// Object literal enhancement
+const autenticado = true;
+const usuario = 'alan';
+const password = 'tugfa2.0';
 const nuevoObjeto = {
-    autenticado : autenticado, //puedes quitar el repetido de autenticado pero ten cuidado deben llamarse igual
-    usuario : usuario,
-    password : password
-}
-console.table(nuevoObjeto)
+    autenticado: autenticado, // you can omit the repeated authenticated, but be careful; they must be named the same
+    usuario: usuario,
+    password: password
+};
+console.table(nuevoObjeto);
