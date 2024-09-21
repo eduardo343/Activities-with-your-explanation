f9375aff6678e5172740019d1d98f77dac3f5eb6
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2024-09-20 20:14:01 -0600

    Update 07.js

--- FundamentosJS/07.js
+++ FundamentosJS/07.js
@@ -1,24 +1,23 @@
-// unir 2 objetos
+// Merging 2 objects
 const producto = {
     nombre: 'Tablet',
     precio: 300,
     disponible: true
-}
+};
 
 const cliente = {
     nombre: 'alan',
-    premium : true
-}
+    premium: true
+};
 
-//vamos a unir a estos dos
+// Let's merge these two
 
-//const nuevoObjeto = Object.assign(producto, cliente) // aqui nos devolvera un nuevo objeto para la consola
+// const nuevoObjeto = Object.assign(producto, cliente); // this will return a new object for the console
 const nuevoObjeto2 = {
-    producto: {...producto},
-    cliente: {...cliente}
-}//los tres puntos significa que tome una prueba del producto.
+    producto: { ...producto },
+    cliente: { ...cliente }
+}; // the three dots mean to take a copy of the product.
 
-
-console.log(nuevoObjeto2)
-console.log(cliente)
-console.log(producto)
+console.log(nuevoObjeto2);
+console.log(cliente);
+console.log(producto);
