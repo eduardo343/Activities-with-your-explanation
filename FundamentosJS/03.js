806ac3dd6716c5cb26004b8323796f92ed0ac16c
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2024-09-20 20:10:57 -0600

    Update 03.js

--- FundamentosJS/03.js
+++ FundamentosJS/03.js
@@ -1,61 +1,61 @@
-//tipos de datos
+// Data Types
 
-/// undefined 
-//let cliente  aqui necesitas definir con algun valor y cambiara a number por el tipo de dato.
-//console.log(cliente);
-//console.log(typeof cliente); aqui salen no definido
+// undefined 
+// let cliente; // here you need to define it with a value, and it will change to number based on the data type.
+// console.log(cliente); // console.log(cliente); 
+// console.log(typeof cliente); // this will show as undefined
 
-//boolean true or false
-//const descuento = false de igual forma si le modificas aqui false o true te dira que es boolean
-//console.log(descuento);
-//console.log(typeof descuento); aqui es donde te saldra que si es boolean
+// boolean true or false
+// const descuento = false; // similarly, if you change this to false or true, it will indicate that it's a boolean
+// console.log(descuento); 
+// console.log(typeof descuento); // this will show that it is a boolean
 
-//number existe una gran cantidad pero a js le da igual
-//const number1 =  20.20
-//const number2 =  40
-//const number3 =  -100
-//console.log(typeof number1);
-//console.log(typeof number2); en number todos son tratados por igual
-//console.log(typeof number3)1;
+// number - there are many types, but JS treats them the same
+// const number1 = 20.20;
+// const number2 = 40;
+// const number3 = -100;
+// console.log(typeof number1); 
+// console.log(typeof number2); // all numbers are treated the same
+// console.log(typeof number3); 
 
-//strings o cadenas de texto
-//const alumno = "pedro" //algunas veces si comienzas con ' sencilla " y terminas con una doble oveamente te saldra error
-//let producto = "television de 70 pulgadas"
-//let producto2 = 'teclado gamer'
+// strings or text strings
+// const alumno = "pedro"; // sometimes if you start with a single quote ' and end with a double quote ", it will give an error
+// let producto = "television de 70 pulgadas"; 
+// let producto2 = 'teclado gamer';
 
-//const numero = '30' // cuando usas las '' como en este ejemplo te dira que es string
-//const numero2 = 40 // cuando no usas '' te lo detecta como un numero
+// const numero = '30'; // when you use '' as in this example, it will indicate that it's a string
+// const numero2 = 40; // when you don't use '', it recognizes it as a number
 
-//console.log(typeof numero);
-//console.log(typeof numero2);
+// console.log(typeof numero); 
+// console.log(typeof numero2);
 
-//BigInt son para numeros muy pero muy grandes.
-//const numeroGrande = BigInt(121564544646544458899998); // para hacer la suma de numeros muy grandes solo copiar
-//const numeroGrande2 = BigInt(121564544646544458899998);// utilizas el console log para hacer la suma de estos grandes.
+// BigInt is for very, very large numbers.
+// const numeroGrande = BigInt(121564544646544458899998); // to sum very large numbers, just copy
+// const numeroGrande2 = BigInt(121564544646544458899998); // use console.log to sum these large numbers.
 
-//const numero = 5000006
-//const numero2 = 5000006
+// const numero = 5000006; 
+// const numero2 = 5000006;
 
-//console.log(typeof numeroGrande);
-//console.log (typeof numeroGrande2);
+// console.log(typeof numeroGrande); 
+// console.log(typeof numeroGrande2); 
 
-//console.log( numeroGrande + numeroGrande2);//tambien se pueden hacer las sumas de ambas cosas.
-//console.log(numero + numero2);// recuerda que no puedes sumar numero con numerogrande
+// console.log(numeroGrande + numeroGrande2); // you can sum both types. 
+// console.log(numero + numero2); // remember that you cannot sum a number with a BigInt
 
-//const numero = '30'
-//const numero2 = 30
+// const numero = '30'; 
+// const numero2 = 30; 
 
-//console.log (typeof String(numero2));
-//console.log(typeof Number(numero));//por si quieres convertir esta parte en string solo escribe string en donde dice number
+// console.log(typeof String(numero2)); 
+// console.log(typeof Number(numero)); // to convert this part to string, just write String where it says Number
 
-//symbol
-//const primerSymbol = Symbol(30)
-//const segundoSymbol = Symbol(30)
+// symbol
+// const primerSymbol = Symbol(30); 
+// const segundoSymbol = Symbol(30); 
 
-//console.log(primerSymbol === segundoSymbol)// ==== a esto se le llama igualdad estricta
-//console.log(primerSymbol.valueOf());
-//console.log(segundoSymbol.valueOf());
+// console.log(primerSymbol === segundoSymbol); // this is called strict equality
+// console.log(primerSymbol.valueOf()); 
+// console.log(segundoSymbol.valueOf()); 
 
-//Null valor null 
-//const descuento = null  //su instancia sera de tipo objeto  // objeto si sera funcion de objeto
-//console.log(typeof descuento)
+// Null value null 
+// const descuento = null; // its instance will be of type object // object will be an object function
+// console.log(typeof descuento);
