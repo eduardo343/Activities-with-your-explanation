79529ed47f141b8bff0627631cda284cbd47c181
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2024-09-20 20:17:41 -0600

    Update 12.js

--- FundamentosJS/12.js
+++ FundamentosJS/12.js
@@ -1,42 +1,47 @@
-//funciones - function declaration
+// Functions - Function Declaration
 
-//function sumar(){
-    //console.log(2 * 2);
-//}
-//sumar()
+// Simple function declaration
+// function sumar() {
+//     console.log(2 * 2); // Logs the result of 2 * 2
+// }
+// sumar(); // Calls the function
 
+// Function with parameters
+// Parameters are the variables in the function definition
+// function sumar(numero = 0, numero2 = 0) {
+//     console.log(numero2); // Logs the second parameter
+//     console.log(numero + numero2); // Logs the sum of both parameters
+//     // console.log(numero + numero2 + numero3); // This will cause an error because 'numero3' is not defined
+//     console.log(numero); // Logs the first parameter
+//     console.log(numero2); // Logs the second parameter
+//     // console.log(numero3); // This will also cause an error
+// }
 
-//en declaracion de la funcion son parametros
-//function sumar(numero = 0, numero2 = 0){
-    //console.log(numero2)
-   // console.log(numero + numero2)
-    //console.log(numero + numero2 + numero3);
-    //console.log (numero)
-    //console.log(numero2)
-    //console.log(numero3);
-//}
+// Function calls with arguments
+// sumar(10, 30); // Calls the function with arguments 10 and 30
+// sumar(5, 5); // Calls the function with arguments 5 and 5
+// sumar(10.5, 10.5); // Calls the function with decimal arguments
+// sumar(); // Calls the function with default values (0, 0)
 
-//sumar(10, 30)// yo utilice tres digitos para que la suma salga mejor
-//sumar(5, 5)
-//sumar(10.5, 10.5) //los valores de aqui se les llama argumento
-//sumar()
-
-function sumar( numero = 0, numero2 = 0){
+// Function returning an object
+function sumar(numero = 0, numero2 = 0) {
     return { 
-        resultado: numero + numero2, 
-        mensaje: 'Hola mundo' //aqui ya se esta retornando un objeto
+        resultado: numero + numero2, // Returns the sum
+        mensaje: 'Hola mundo' // Returns a message
+    };
 }
-}
 
-const {resultado, mensaje} = sumar(20, 30)
+// Destructuring the returned object
+const { resultado, mensaje } = sumar(20, 30); // Calls the function with arguments 20 and 30
 
-//const {resultado, Holamundo} = sumar(20, 30) // el antiguo resultado
+// Incorrect destructuring (commented out for clarity)
+// const { resultado, Holamundo } = sumar(20, 30); // 'Holamundo' does not exist in the returned object
+// const [resultado, Holamundo] = sumar(20, 30); // This will cause an error because the returned value is an object, not an array
+// const resultado = sumar(10, 50); // This assigns the entire object to 'resultado'
 
-//const [resultado, Holamundo] = sumar(20, 30) // en este te da lo resultados separados
-//const resultado = sumar (10, 50) // en este uso te da la suma junto al hola mundo
-console.log(resultado);
-console.log(mensaje)
-//console.log(Holamundo); 
+console.log(resultado); // Logs the sum (50)
+console.log(mensaje); // Logs 'Hola mundo'
+// console.log(Holamundo); // This would cause an error because 'Holamundo' is not defined
 
-//jsx react
-//const [ resultado, setResultado] = useState(0)
+// Example in JSX (React)
+// const [resultado, setResultado] = useState(0); // Using React's useState hook to manage state
