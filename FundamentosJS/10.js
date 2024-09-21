7f864403711ad2a0ed53b4a50f938dc3fd24dc75
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2024-09-20 20:16:10 -0600

    Update 10.js

--- FundamentosJS/10.js
+++ FundamentosJS/10.js
@@ -1,17 +1,18 @@
-//Destructuring de arrays
-const tecnologias = ['Html', 'React', 'JavaScript', 'Node.js']
+// Destructuring Arrays
+const tecnologias = ['Html', 'React', 'JavaScript', 'Node.js'];
 
-const [, , , JavaScript ] = tecnologias
+// Destructuring with skipped elements
+const [, , , JavaScript] = tecnologias; // Skips the first three elements and assigns the fourth to the variable 'JavaScript'.
 
-console.log( Nodejs)
+// Note: The variable 'Nodejs' is not defined, so this will cause an error. It should be 'Node.js'.
+// console.log(Nodejs); // This line will throw an error because 'Nodejs' is not declared.
 
+// Uncommenting the following code demonstrates destructuring an object:
 
+// const cliente = {
+//     nombre: 'alan',
+//     tipo: 'premium'
+// };
 
-
-//const cliente = {
-    //nombre: 'alan',
-  //  tipo: 'premium'
-//}
-
-//const {tipo} = cliente
-//console.log(tipo);
+// const { tipo } = cliente; // Destructures 'tipo' from the 'cliente' object.
+// console.log(tipo); // Prints 'premium'.
