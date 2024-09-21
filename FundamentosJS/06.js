1cde62ae7ded3f434e06a4dbe59e12d4fe9b8557
Alan Eduardo Ugarte Saucedo <54459451+eduardo343@users.noreply.github.com>
2024-09-20 20:13:25 -0600

    Update 06.js

--- FundamentosJS/06.js
+++ FundamentosJS/06.js
@@ -1,24 +1,24 @@
-//destructuring de 2 o mas objetos
+// Destructuring of 2 or more objects
 const producto = {
     nombre: "tablet",
     precio: 300,
     disponible: true
- };
- 
- const cliente = {
- 
-       nombre: "alan",//aqui esta modificado el nombre como lo notaras
-       premium: true
- };
- 
- const { nombre, precio, disponible} = producto
- //const { nombreC, premium } =  cliente// aqui declaras el nuevo nombre yo le puse nombre C
-//Existe un metodo mas rapido para declarar que seria este
+};
 
-const { nombre: nombreC,premium} = cliente
+const cliente = {
+    nombre: "alan", // here the name has been modified, as you will notice
+    premium: true
+};
 
- console.log(nombre, precio, disponible) // copias lo mismo y lo pones abajo
- //al quitar la variables precio disponible solo va aparecer el nombre tablet
+// Destructuring from producto
+const { nombre, precio, disponible } = producto;
+// const { nombreC, premium } = cliente; // here you declare a new name; I named it nombreC
 
- console.log(nombreC, premium) = cliente// aqui abajo debes crear un nuevo cliente yo cree nombreC
- //Tambien es el caso de aqui si quitas premium solo saldra el nombre del usuario
+// A faster way to declare is this
+const { nombre: nombreC, premium } = cliente;
+
+console.log(nombre, precio, disponible); // copies the same values and prints them below
+// if you remove the variables precio and disponible, only the name "tablet" will appear
+
+console.log(nombreC, premium); // here you should create a new cliente; I created nombreC
+// Similarly, if you remove premium, only the user's name will be displayed
